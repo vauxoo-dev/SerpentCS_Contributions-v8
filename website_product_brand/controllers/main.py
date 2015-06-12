5f880c0c53baad68239ddc1516a414943f84d522
oalca <oszckar@gmail.com>
2015-06-12 11:58:38 -0500

    [ADD] noqa to let only one line

--- website_product_brand/controllers/main.py
+++ website_product_brand/controllers/main.py
@@ -217,8 +217,7 @@ class website_sale(openerp.addons.website_sale.controllers.main.website_sale):
             uid,
             category_ids,
             context=context)
-        categs = filter(
-            lambda x: not x.parent_id, categories)  # pylint: disable=W0141
+        categs = filter(lambda x: not x.parent_id, categories)  # pylint: disable=W0141 # noqa
         if category:
             selected_id = int(category)
             child_prod_ids = category_obj.search(
